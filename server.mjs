86a5b27b16dbd22438c8c1ab57f2a9e975b065ce
Lavanya <lavanyap@redisolve.com>
2025-03-18 12:09:39 +0530

    require options mehtod

--- server.mjs
+++ server.mjs
@@ -1,14 +1,75 @@
 
+//THIS IS AN EXAMPLE OF IMPORT FILE FS, I HAVE USED THE require OPTION IN ANOTHER SERVER1.MJS SEE THERE
+
+//in require file you cannot use the server.mjs extensio, so save as server1.js
+
 //go to node.js documentation and copy the readfile and paste here, 
 //in the documentation itself we have various factors
 
 import { readFile } from 'node:fs';
 
+// 1 . option 1
+
 //readFile('/etc/passwd', (err, data) => {
-  readFile('./files/start.txt', (err, data) => {
+  readFile('./files/start.txt', (err, data) => { 
   if (err) throw err;
   console.log(data.toString()); //convert to tostring to get the values correctly instead of seeing the buffer data
+
 });
+
+
+// 2. option 2
+
+readFile('./files/start.txt', 'utf8',(err, data) => { //utf8 is the data will be read and sent in a readable form, see the documentation,utf8 is the encoding to read the data,
+  if (err) throw err;
+  console.log(data); //when you use utf 8 ,you need not give the tostring in the console , so you can either choose option 1 or 2
+
+});
+
+
+
+console.log("hiiiiiiiiiiiii")  //------------ read the data asynchrounously in node.js, 1st it prints the hi and then display the below exception
+//PS C:\Users\lavanyap\Desktop\Node js\samplefolder> node server.mjs
+//hiiiiiiiiiiiii
+//there was an uncaught error: Error: ENOENT: no such file or directory, open 'C:\Users\lavanyap\Desktop\Node js\samplefolder\files\startrtretre.txt'
+//PS C:\Users\lavanyap\Desktop\Node js\samplefolder> 
+
+
+//exit on uncaught errors
+//if there is an exception ,this part of the code gets executed
+//process is the default keyword of node
+process.on('uncaughtException',err => {
+  console.log(`there was an uncaught error: ${err}`)
+  process.exit(1)
+})
+
+//code with error to test the above options, here i changed the path of the filenam as strttter, so this the eror
+
+readFile('./files/startrtretre.txt', 'utf8',(err, data) => { //utf8 is the data will be read and sent in a readable form, see the documentation,utf8 is the encoding to read the data,
+  if (err) throw err;
+  console.log(data); //when you use utf 8 ,you need not give the tostring in the console , so you can either choose option 1 or 2
+
+});
+//PS C:\Users\lavanyap\Desktop\Node js\samplefolder> node server.mjs
+//there was an uncaught error: Error: ENOENT: no such file or directory, open 'C:\Users\lavanyap\Desktop\Node js\samplefolder\files\startrtretre.txt'
+//PS C:\Users\lavanyap\Desktop\Node js\samplefolder> 
+
+
+
+
+
+
+//DIFFERENCE BETWEEN FS AND REQUIRE
+
+// 1. if you use fs - import { readFile } from 'node:fs';
+// the code will run specifically for the readfile alone and other functions does not work
+// if you instead give in require , all functions of fs can be called and executed, like fs.readfile, fs.writefile etc.
+
+
+//notes
+
+//node.js reads file asynchronously the entire content
+
 
 //in node.js documentation, go to "file system" and search read files , you see fs.readfile, in that
 //click that and see the codes there--you will see all the necessary code for reading the file
---
+++ server1.js
@@ -0,0 +1,62 @@
+
+
+const fs =require('fs')
+const path=require('path')
+
+
+// 1. READFILE USING FS
+
+ fs.readFile(path.join(__dirname,'files','start.txt'),'utf8', (err, data) => { 
+  if (err) throw err;
+  console.log(data.toString()); 
+
+});
+
+
+//2 .WRITE FILE USING FS
+
+//after executing the below option ,you will see the write.txt gets added under files folder
+
+fs.writeFile(path.join(__dirname,'files','writefile.txt'),'Hi this is a write file,here we give the content by writing', (err) => { 
+  if (err) throw err;
+  console.log('write complete')
+
+});
+
+
+
+// 3. UPDATE THE WRITE FILE USING APPEND METHOD
+
+//inside the writetxt the below content gets updated to the existing line
+fs.appendFile(path.join(__dirname,'files','writefile.txt'),'\n\n Thanks for updating', (err) => { 
+  if (err) throw err;
+  console.log('append complete')
+
+});
+
+
+
+//4. you can add a file using the append method ---- adding a new append file under files folder
+fs.appendFile(path.join(__dirname,'files','appendfile.txt'),'creating a new append file', (err) => { 
+  if (err) throw err;
+  console.log('append complete')
+});
+
+
+// 5. RENAMING THE EXISTING FILE---renaming the append file to "rename file"
+//after this code the append file name gets changed to rename
+
+fs.rename(path.join(__dirname,'files','appendfile.txt'),path.join(__dirname,'files','Rename.txt'), (err) => { 
+  if (err) throw err;
+  console.log('append complete')
+});
+
+
+
+
+// OUTPUT
+
+//PS C:\Users\lavanyap\Desktop\Node js\samplefolder> node server1
+//hi this is a file created inside the files folder, this is a data file which have the datas
+
+//so now go to server.mjs and give this file path there to access this file in the output  
